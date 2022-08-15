06bcee001b6f3bc15205591480762cb6e34b6416
Jaden Lewis-Chambers <you@example.com>
2022-08-15 00:25:05 -0400

    fixing the app

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export default [
     {
         id: 1,
-        title: "Why are Strats Good For You?",
-        desc: "Les Pauls, Danelectro's, and Rickenbacker's. Nothing comes close to Fender guitars..",
+        title: "Why Are Strats Good For You?",
+        desc: "Les Pauls, Danelectro's, and Rickenbacker's. Nothing comes close to Fender guitars",
         dateTime: "Aug 10",
         readTime: "7 min read",
         tag: "Music",
@@ -12,7 +12,7 @@ export default [
     {
         id: 2,
         title: "The Pandemic: What's Next?",
-        desc: "Covid-19 spawned a revolution in understanding how some common viruses spread and how we can stop them.",
+        desc: "Covid-19 spawned a revolution in understanding how some common viruses spread and how we can stop them",
         dateTime: "Aug 2",
         readTime: "12 min read",
         tag: "Health",
@@ -20,8 +20,8 @@ export default [
     },
     {
         id: 3,
-        title: "6 of the top remaining 2022 NBA free agents",
-        desc: "Covid-19 spawned a revolution in understanding how some common viruses spread and how we can stop them.",
+        title: "6 of The Top Remaining 2022 NBA Free Agents",
+        desc: "Covid-19 spawned a revolution in understanding how some common viruses spread and how we can stop them",
         dateTime: "July 29",
         readTime: "5 min read",
         tag: "Sports",
@@ -38,12 +38,57 @@ export default [
     },
     {
         id: 5,
-        title: "The title of this will change",
-        desc: "What to do...",
-        dateTime: "July 24",
-        readTime: "5 min read",
+        title: "NFL Preseason Week 1 Winners and Losers",
+        desc: "Rookie quarterbacks impress, Deshaun Watson struggles in Browns debut",
+        dateTime: "August 14",
+        readTime: "8 min read",
         tag: "Sports",
-        image: ""
+        image: "https://sportshub.cbsistatic.com/i/r/2022/08/14/b6eb829e-dd8c-4703-98df-00f0d5efc1f3/thumbnail/770x433/109debdeff2fb54eb572d13772a57959/picketno1.jpg"
+    },
+    {
+        id: 6,
+        title: "Health Care Reform Looking Imminent",
+        desc: "Big changes are coming for health care costs",
+        dateTime: "August 1",
+        readTime: "4 min read",
+        tag: "Health",
+        image: "https://static01.nyt.com/images/2022/08/13/opinion/13levitt_1H/13levitt_1H-superJumbo.jpg?quality=75&auto=webp"
+    },
+    {
+        id: 7,
+        title: "Fleet Foxes and Blood Orange Cover the Strokes’ “Under Control”: Watch",
+        desc: "Both performed the track live at their respective headlining shows in New York City this weekend",
+        dateTime: "August 14",
+        readTime: "4 min read",
+        tag: "Music",
+        image: "https://media.pitchfork.com/photos/62f9401251420cb2c6a10bd7/master/pass/Fleet-foxes-blood-orange-the-strokes-cover-under-control-nyc-getty-video.png"
+    },
+    {
+        id: 8,
+        title: "Loveless: A Contemporary Review",
+        desc: "We dive deep to understand what makes My Bloody Valentine's sophmore album so timeless",
+        dateTime: "July 26",
+        readTime: "4 min read",
+        tag: "Music",
+        image: "https://pbs.twimg.com/media/EqTo2onXMAEQ-h0.jpg"
+    },
+    {
+        id: 9,
+        title: "Why Are Dragon Fruit Making a Resurgence?",
+        desc: "Dragon fruit, a popular fruit originating from Central and South America is becoming a national trend",
+        dateTime: "July 12",
+        readTime: "3 min read",
+        tag: "Health",
+        image: "https://www.producereport.com/sites/default/files/styles/large/public/field/image/5016311514_569c3cb65b_k_0.jpg?itok=E4YoPI89"
+    },
+    {
+        id: 10,
+        title: "Florida Begins Rolling Out Monkeypox Vaccines",
+        desc: "In an effort to give everyone a chance at getting the shot, Florida focuses on first doses",
+        dateTime: "August 11",
+        readTime: "6 min read",
+        tag: "Health",
+        image: "https://images.unsplash.com/photo-1624024093647-c240f7944d87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHZhY2NpbmF0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
 ]
 
